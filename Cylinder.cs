64d47f8a9d27afa6a4907c01ecac67f62b3a25ac
bakagaigin <differenceclouds@gmail.com>
2024-01-25 09:06:58 -0500

    Initial commit

---
+++ Cylinder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using static System.Math;
+
+namespace Castelian {
+	public class Cylinder {
+
+		int Radius;
+		int sections;
+		int[] coords;
+		double angle;
+		double speed;
+		const double RAD = Math.PI / 180.0;
+		int crop;
+
+		Texture2D wallTile;
+		Texture2D background;
+
+
+		public Cylinder(int _width, int _sections, Texture2D _texture, Texture2D _background) {
+			wallTile = _texture;
+			background = _background;
+			Radius = _width / 2;
+			sections = _sections;
+			coords = new int[sections];
+			angle = 360.0 / sections;
+			speed = 30.0;
+
+			double halfSide = Radius * (Sin((angle / 2.0) * RAD) / Sin(90.0 * RAD));
+			int minRadius = (int)Sqrt(Radius * Radius - halfSide * halfSide);
+			Console.WriteLine("minRadius: " + minRadius + ", halfside: " + halfSide);
+		}
+
+		public double ViewAngle {
+			get; private set;
+		}
+
+
+		public void Update(GameTime gameTime, Point cursor) {
+			double t = gameTime.TotalGameTime.TotalSeconds * speed;
+			double offset = cursor.X / 5.0;
+
+			ViewAngle = t % 360;
+			//ViewAngle = offset;
+
+			for (int i = 0; i < sections; i++) {
+				coords[i] = (int)(Sin((i * angle + ViewAngle) * RAD) * Radius);
+				//coords[i] = (int)(Sin((i * angle + ViewAngle) * RAD) * Radius);
+			}
+		}
+
+		Rectangle window = new(0, 0, 1024, 768);
+
+		public void Draw(SpriteBatch spriteBatch) {
+			int bgPos = (int)((background.Width / 360.0) * -ViewAngle);
+			Rectangle bgView = new Rectangle(bgPos, 0, window.Width, window.Height);
+			spriteBatch.Draw(background, window, bgView, Color.White);
+
+			for(int i = 0; i < coords.Length; i++) {
+				int x1 = 512 + coords[i];
+				int x2 = 512 + coords[(i + 1) % coords.Length];
+
+				Point pos = new(x1, 0);
+				Point size = new(x2 - x1, 768);
+
+				Rectangle source = new Rectangle(-16, -16, wallTile.Width, 768);
+				Rectangle destination = new Rectangle(pos, size);
+
+				//Color color = i % 2 == 0 ? Color.White : Color.DarkBlue;
+
+				spriteBatch.Draw(wallTile, destination, source, Color.White);
+
+				int y = i * 32 - 32;
+				while(y < 768) {
+					Rectangle stripe = new(x1, y, x2 - x1, 64);
+					y += 32 * sections;
+					Primitives2D.FillRectangle(spriteBatch, stripe, Color.DarkBlue);
+				}
+				
+
+				
+			}
+		}
+
+	}
+}
---
+++ Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Castelian {
+	public class Game1 : Game {
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		public int WindowWidth { get; private set; }
+		public int WindowHeight { get; private set; }
+
+		public Game1() {
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+
+			WindowWidth = 1024;
+			WindowHeight = 768;
+
+			_graphics.PreferredBackBufferWidth = WindowWidth;
+			_graphics.PreferredBackBufferHeight = WindowHeight;
+			_graphics.ApplyChanges();
+
+			base.Initialize();
+		}
+
+		protected override void Initialize() {
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+
+
+
+		Cylinder cylinder;
+
+		protected override void LoadContent() {
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			Texture2D cylinderTexture = Content.Load<Texture2D>("bluebrick");
+			Texture2D background = Content.Load<Texture2D>("internetandrej_1_looped");
+			cylinder = new(656, 16, cylinderTexture, background);
+		}
+
+		protected override void Update(GameTime gameTime) {
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			MouseState mouseState = Mouse.GetState();
+			Point cursor = new(mouseState.X, mouseState.Y);
+
+
+			cylinder.Update(gameTime, cursor);
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.Black);
+
+			_spriteBatch.Begin(samplerState: SamplerState.AnisotropicWrap);
+			cylinder.Draw(_spriteBatch);
+			_spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
